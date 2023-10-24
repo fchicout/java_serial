30375732273533bb1545735f3285f8d25b3b966c
Fábio Chicout <fchicout@gmail.com>
2023-10-23 21:50:40 -0300

    modularizando o comunicador serial

--- src/main/java/br/edu/uniaeso/App.java
+++ src/main/java/br/edu/uniaeso/App.java
@@ -4,103 +4,44 @@ import com.fazecast.jSerialComm.SerialPort;
 
 public class App {
 
-    public static void getPortsInfo() {
-        System.out.println("\n\n Available Ports");
-        SerialPort[] ports = SerialPort.getCommPorts();
-        for (SerialPort serialPort : ports) {
-            System.out.println("Descriptive Port Name:\t" + serialPort.getDescriptivePortName());
-            System.out.println("Is Open:\t\t" + serialPort.isOpen());
-            System.out.println("Description:\t\t" + serialPort.getPortDescription());
-            System.out.println("Location:\t\t" + serialPort.getPortLocation());
-            System.out.println("Serial Number:\t\t" + serialPort.getSerialNumber());
-            System.out.println("Port Name:\t\t" + serialPort.getSystemPortName());
-            System.out.println("Port Path:\t\t" + serialPort.getSystemPortPath());
-            System.out.println();
-        }
-    }
-
-    public static void closeAllPorts() {
-        SerialPort[] ports = SerialPort.getCommPorts();
-        for (SerialPort serialPort : ports) {
-            if (serialPort.isOpen()) {
-                serialPort.flushIOBuffers();
-                serialPort.closePort();
-            }
-        }
-    }
-
-    public static SerialPort getCOMMPortByName(String portName) {
-        SerialPort[] ports = SerialPort.getCommPorts();
-        for (SerialPort serialPort : ports) {
-            if (serialPort.getSystemPortName().equals(portName)) {
-                return serialPort;
-            }
-        }
-        return null;
-    }
-
     public static void main(String[] args) throws InterruptedException {
-
-        int BaudRate = 115200;
-        int DataBits = 8;
-        int StopBits = SerialPort.ONE_STOP_BIT;
-        int Parity = SerialPort.NO_PARITY;
+        ArduinoSerialCommunicator asc = new ArduinoSerialCommunicator(115200, 8, SerialPort.ONE_STOP_BIT,
+                SerialPort.NO_PARITY);
         System.out.println("\n\n SerialPort Data Transmission");
 
         // use the for loop to print the available serial ports
-        getPortsInfo();
+        asc.getPortsInfo();
         System.out.println("Closing ports");
-        closeAllPorts();
+        asc.closeAllPorts();
 
         // Open the first Available port
-        SerialPort commPort = getCOMMPortByName("COM6");
+        SerialPort commPort = asc.getCOMMPortByName("COM6");
 
-        // Set Serial port Parameters
-        commPort.setComPortParameters(BaudRate, DataBits, StopBits, Parity);// Sets all serial port parameters at
-                                                                            // one time
+        asc.setSerialParameters();
 
         commPort.openPort(2000); // open the port
         // Arduino Will Reset
         System.out.println(" Watch Arduino for Reset ");
 
-        if (commPort.isOpen()) {
-            System.out.println("\n" + commPort.getSystemPortName() + "  is Open ");
-        } else {
+        if (!asc.verifyPortOpen()) {
             System.out.println(" Port not open ");
             System.exit(-1);
         }
 
-        // Display the Serial Port parameters
-        System.out.println("\n Selected Port               = " + commPort.getSystemPortName());
-        System.out.println(" Selected Baud rate          = " + commPort.getBaudRate());
-        System.out.println(" Selected Number of DataBits = " + commPort.getNumDataBits());
-        System.out.println(" Selected Number of StopBits = " + commPort.getNumStopBits());
-        System.out.println(" Selected Parity             = " + commPort.getParity());
+        asc.showSelectedPortInfo();
 
         Thread.sleep(2000); // Delay introduced because when the SerialPort is opened ,Arduino gets resetted
                             // Time for the code in Arduino to rerun after Reset
 
-        try {
-            byte[] WriteByte = "on".getBytes();
+        asc.writeMessage("on");
 
-            int bytesTxed = 0;
+        
+        asc.closeCOMMPort(); // Close the port
 
-            bytesTxed = commPort.writeBytes(WriteByte, WriteByte.length);
-
-            System.out.print(" Bytes Transmitted -> " + bytesTxed);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!asc.verifyPortOpen()) {
+            System.out.println(" Port not open ");
+            System.exit(-1);
         }
-
-        commPort.closePort(); // Close the port
-
-        if (commPort.isOpen())
-            System.out.println(commPort.getSystemPortName() + " is Open ");
-        else
-            System.out.println("\n Port not open ");
-
-        commPort.flushIOBuffers();
     }
 
 }
---
+++ src/main/java/br/edu/uniaeso/ArduinoSerialCommunicator.java
@@ -0,0 +1,134 @@
+package br.edu.uniaeso;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class ArduinoSerialCommunicator {
+    private int baudRate, dataBits, stopBits, parity;
+    private SerialPort commPort;
+
+    public SerialPort getCommPort() {
+        return commPort;
+    }
+
+    public void setCommPort(SerialPort commPort) {
+        this.commPort = commPort;
+    }
+
+    public int getBaudRate() {
+        return baudRate;
+    }
+
+    public void setBaudRate(int baudRate) {
+        this.baudRate = baudRate;
+    }
+
+    public int getDataBits() {
+        return dataBits;
+    }
+
+    public void setDataBits(int dataBits) {
+        this.dataBits = dataBits;
+    }
+
+    public int getStopBits() {
+        return stopBits;
+    }
+
+    public void setStopBits(int stopBits) {
+        this.stopBits = stopBits;
+    }
+
+    public int getParity() {
+        return parity;
+    }
+
+    public void setParity(int parity) {
+        this.parity = parity;
+    }
+
+    public ArduinoSerialCommunicator(int baudRate, int dataBits, int stopBits, int parity) {
+        setBaudRate(baudRate);
+        setDataBits(dataBits);
+        setStopBits(stopBits);
+        setParity(parity);
+
+    }
+    
+    public void setSerialParameters(){
+        // Set Serial port Parameters
+        // parameters at one time
+        getCommPort().setComPortParameters(getBaudRate(), getDataBits(), getStopBits(), getParity());// Sets all serial port
+
+    }
+
+    public boolean verifyPortOpen(){
+        if (getCommPort().isOpen()) {
+            System.out.println("\n" + commPort.getSystemPortName() + " is Open ");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void showSelectedPortInfo(){
+        // Display the Serial Port parameters
+        System.out.println("\n Selected Port               = " + getCommPort().getSystemPortName());
+        System.out.println(" Selected Baud rate          = " + getCommPort().getBaudRate());
+        System.out.println(" Selected Number of DataBits = " + getCommPort().getNumDataBits());
+        System.out.println(" Selected Number of StopBits = " + getCommPort().getNumStopBits());
+        System.out.println(" Selected Parity             = " + getCommPort().getParity());
+    }
+
+    public int writeMessage(String msg){
+        int bytesTxed = 0;
+        try {
+            byte[] writeByte = msg.getBytes();
+            bytesTxed = commPort.writeBytes(writeByte, writeByte.length);
+            System.out.print(" Bytes Transmitted -> " + bytesTxed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bytesTxed;
+    }
+
+    public void getPortsInfo() {
+        System.out.println("\n\n Available Ports");
+        SerialPort[] ports = SerialPort.getCommPorts();
+        for (SerialPort serialPort : ports) {
+            System.out.println("Descriptive Port Name:\t" + serialPort.getDescriptivePortName());
+            System.out.println("Is Open:\t\t" + serialPort.isOpen());
+            System.out.println("Description:\t\t" + serialPort.getPortDescription());
+            System.out.println("Location:\t\t" + serialPort.getPortLocation());
+            System.out.println("Serial Number:\t\t" + serialPort.getSerialNumber());
+            System.out.println("Port Name:\t\t" + serialPort.getSystemPortName());
+            System.out.println("Port Path:\t\t" + serialPort.getSystemPortPath());
+            System.out.println();
+        }
+    }
+
+    public void closeAllPorts() {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        for (SerialPort serialPort : ports) {
+            if (serialPort.isOpen()) {
+                serialPort.flushIOBuffers();
+                serialPort.closePort();
+            }
+        }
+    }
+
+    public SerialPort getCOMMPortByName(String portName) {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        for (SerialPort serialPort : ports) {
+            if (serialPort.getSystemPortName().equals(portName)) {
+                setCommPort(serialPort);
+                return serialPort;
+            }
+        }
+        return null;
+    }
+
+    public void closeCOMMPort() {
+        getCommPort().flushIOBuffers();
+        getCommPort().closePort();
+    }
+}
